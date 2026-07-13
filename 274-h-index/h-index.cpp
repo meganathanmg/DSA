dcd992003493304cd159cedb4e0bda21a0d66508
Meganathan <140222790+meganathanmg@users.noreply.github.com>
2026-07-13 23:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (34.79%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n=citations.size(),h=0;
-        for(int i=1;i<=n;i++){
-            int cnt=0;
-            for(int j=0;j<n;j++){
-                if(citations[j]>=i) cnt++;
+        int n = citations.size();
+        sort(citations.begin(), citations.end());
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
             }
-            if(cnt>=i) h=i;
         }
-        return h;
+        return 0;
     }
 };
